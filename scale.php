9f4f2496e2f48a595079ef3b9342574a5df3d64c
Karan-14 <59825591+Karan-14@users.noreply.github.com>
2020-04-16 16:09:36 +0200

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -4,8 +4,6 @@
 	$rechts = 5;
 
 if ($links == $rechts) {	
-		echo "in balans";	
-    }
+		echo "in balans";} 
 elseif ($links > $rechts) {	
-		echo "niet in balans";	
-    }
+		echo "niet in balans";}
