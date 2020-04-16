937d0397f1c53541aadf2b822fe8d226a51d2c84
Karan-14 <59825591+Karan-14@users.noreply.github.com>
2020-04-16 16:02:33 +0200

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -2,11 +2,10 @@
 
 	$links = 7;	
 	$rechts = 5;
-	$cijfers = array("1,2,3,4,5,6,7,8,9");
 
-	if($links == $rechts) {	
+if ($links == $rechts) {	
 		echo "in balans";	
-}
-elseif($links > $rechts) {	
+    }
+elseif ($links > $rechts) {	
 		echo "niet in balans";	
-}
+    }
